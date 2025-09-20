8ece55bb82b15cfa692e0d30c16754c28fc532f9
ianv49 <ianvallejo@yahoo.com>
2025-09-20 18:28:05 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const cities = ['Manila', 'Tokyo', 'New York', 'London'];
-const apiKey = 'wind1'; // Replace with your actual key if needed
-
+// const apiKey = 'wind1'; // Replace with your actual key if needed
+// https://api.openweathermap.org/data/2.5/weather?q=CityName&appid=YOUR_API_KEY
+// api key solar1 e645925cfe8367841ad656678b7c3acc
+// api key wind1 0723d71a05e58ae3f7fc91e39a901e6b
+const apiKey = '0723d71a05e58ae3f7fc91e39a901e6b'; // Replace with your actual key if needed
 const tableBody = document.querySelector('#weatherTable tbody');
 
 cities.forEach(city => {
